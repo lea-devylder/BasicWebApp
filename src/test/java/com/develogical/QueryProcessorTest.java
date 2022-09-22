80d30cd71db72a2e7a77c0a8f1a658e9f52dca74
Lea De Vylder <ldevylde@andrew.cmu.edu>
2022-09-22 11:16:08 -0400

    Added query

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -25,4 +25,9 @@ public class QueryProcessorTest {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
 
+    @Test
+    public void knowsName() throws Exception {
+        assertThat(queryProcessor.process("name"), containsString("lea-query"));
+    }
+
 }
